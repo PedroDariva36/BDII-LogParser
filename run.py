a0e633fc654ec2e6b5fbbeceee8d392da7f13d8a
Pedro Dariva <47556893+PedroDariva36@users.noreply.github.com>
2023-05-31 15:18:34 -0300

    adding a reset for the data

--- run.py
+++ run.py
@@ -1,15 +1,35 @@
+from ast import Delete
 import re
 import psycopg2
+import json
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="postgres",
-    user="postgres",
-    password="postgres")
+def connUp():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="postgres")
+    return conn
 
+def getMetadata():
+    file = open("metadado.json")
+    metadata = json.load(file)
+    return metadata["INITIAL"]
+
+conn = connUp()
 cursor = conn.cursor()
+cursor.execute("delete from exemplo where 1=1")
+conn.commit()
 
-cursor.execute('select * from public.exemplo;')
-result = cursor.fetchall()
-print(result)
+a = getMetadata()
+for i in range(len(a["A"])):
+    s = 'insert into public.exemplo values ({},{},{});'.format(i+1, a["A"][i], a["B"][i])
+    print(s)
+    cursor.execute(s)
+conn.commit()
+result = cursor.close()
+
+
+conn.close()
+#print(result)
 
